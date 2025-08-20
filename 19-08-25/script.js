a403595c09a1860f7d5d3f62fbdc4015bba8c106
harshita280905 <harshitasri.r@gmail.com>
2025-08-20 09:58:38 +0530

    Add files via upload

---
+++ 19-08-25/script.js
@@ -0,0 +1,10 @@
+// Smooth scrolling for sidebar links in future (if added)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
